2a47039497ad65efcf4a480f3823d41b6feaa36f
Chris McKnight <tenlongfingers10@gmail.com>
2022-05-29 16:56:06 -0600

    Oracle half works, need to troubleshoot later

--- server/controller.js
+++ server/controller.js
@@ -1,13 +1,32 @@
 module.exports = {
+  getCompliment: (req, res) => {
+    const compliments = [
+      "Gee, you're a smart cookie!",
+      "Cool shirt!",
+      "Your Javascript skills are stellar.",
+    ];
 
-    getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
-      
-        // choose random compliment
-        let randomIndex = Math.floor(Math.random() * compliments.length);
-        let randomCompliment = compliments[randomIndex];
-      
-        res.status(200).send(randomCompliment);
-    }
+    // choose random compliment
+    let randomIndex = Math.floor(Math.random() * compliments.length);
+    let randomCompliment = compliments[randomIndex];
 
-}
+    res.status(200).send(randomCompliment);
+  },
+
+  getFortune: (req, res) => {
+    const fortunes = [
+      "A lifetime of happiness lies ahead of you.",
+      "Any day above ground is a good day.",
+      "Disbelief destroys the magic.",
+      "Every flower blooms in its own sweet time.",
+      "Go take a rest; you deserve it.",
+      "You can see a lot just by looking.",
+    ];
+
+    // choose random fortune
+    let randomIndex = Math.floor(Math.random() * fortunes.length);
+    let randomFortune = fortunes[randomIndex];
+
+    res.status(200).send(randomFortune);
+  },
+};
